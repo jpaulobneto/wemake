063265d8cbf9e0fa59a76d71d6623c190d19d183
João Paulo Barbosa <jpaulobneto@gmail.com>
2022-12-10 17:09:49 -0300

    refactor: move theme from pages folder to avoid next error

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '@fontsource/raleway/300.css'
 import '@fontsource/raleway/700.css'
 import '@fontsource/sora/400.css'
 import '@fontsource/sora/700.css'
-import theme from './theme'
+import theme from '/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
