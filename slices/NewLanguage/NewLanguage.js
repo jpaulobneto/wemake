69798c34e48f46ea48bed2fd3848f3a9511f7c80
João Paulo <jpaulobneto@gmail.com>
2022-12-16 18:20:05 -0300

    feat: create new language section (#16)

---
+++ slices/NewLanguage/NewLanguage.js
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Highlight,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+
+export default function NewLanguage() {
+  return (
+    <Box as="section">
+      <Flex
+        bg={'url(/new-language-bg.jpg) center center no-repeat'}
+        bgSize="cover"
+        height={{ lg: '790px' }}
+        justify="center"
+        align={'center'}
+      />
+
+      <Container maxW={'container.xl'} py={28} align="center">
+        <Heading as="h1" variant="h1Highlighted" mb={14}>
+          <Highlight
+            query={'nova língua'}
+            styles={{
+              bg: 'brand.100',
+              fontWeight: 'bold',
+              p: 1,
+            }}
+          >
+            Aprendendo uma nova língua
+          </Highlight>
+        </Heading>
+
+        <VStack spacing={8}>
+          <Text>
+            Assim como o estudo das línguas originais é importante para o
+            aprendizado de qualquer idioma, o estudo da{' '}
+            <Text as="strong" color="brand.500">
+              lógica de programação
+            </Text>{' '}
+            e{' '}
+            <Text as="strong" color="brand.500">
+              codificação
+            </Text>{' '}
+            fornece as bases e fundamentos para que qualquer linguagem de
+            programação seja aprendida, rompendo as estruturas pragmatistas da
+            modernidade.
+          </Text>
+          <Text>
+            Dominar uma nova língua, ainda que digital, abrirá caminhos para o
+            <Text as="strong">exercício da vocação com excelência</Text>,
+            independentemente da área de atuação que os nossos alunos sigam.
+            Para mais, <Text as="strong">outras habilidades importantes</Text>{' '}
+            são desenvolvidas através das aulas de Programação e codificação:
+            pensamento computacional, lógica, planejamento, depuração,
+            organização, criatividade, etc.
+          </Text>
+        </VStack>
+      </Container>
+    </Box>
+  )
+}
---
+++ slices/NewLanguage/index.js
@@ -0,0 +1,1 @@
+export { default as NewLanguage } from './NewLanguage'
