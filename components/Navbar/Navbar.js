e14750d00ada66c7550577519ad07f07332252e3
João Paulo Barbosa <jpaulobneto@gmail.com>
2022-12-17 15:56:27 -0300

    refactor: ajdust navbar responsiveness

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -5,22 +5,36 @@ export default function Navbar() {
   return (
     <Container maxWidth="container.xl">
       <Grid
-        templateColumns={{ lg: 'auto 1fr auto', base: 'auto auto' }}
+        templateColumns={[
+          'auto auto',
+          'auto auto',
+          'auto auto',
+          'auto 1fr auto',
+        ]}
+        justifyContent="space-between"
+        alignItems="center"
         gap={8}
         height={20}
       >
-        <GridItem width={40} alignSelf="center" justifySelf="start">
+        <GridItem>
           <Link href="/">
             <Image
               src="/wemake-logo.png"
               alt="We Make - nós criamos"
               width={135}
               height={45}
+              minW={135}
             />
           </Link>
         </GridItem>
-        <GridItem alignSelf="center" display={{ base: 'none', lg: 'grid' }}>
-          <Grid templateColumns="repeat(5, auto)" gap={9}>
+
+        <GridItem display={['none', 'none', 'none', 'block']}>
+          <Grid
+            templateColumns="repeat(5, auto)"
+            justifyContent="space-around"
+            alignItems="center"
+            gap={8}
+          >
             <GridItem>
               <Link href="/home">Quem Somos</Link>
             </GridItem>
@@ -38,16 +52,12 @@ export default function Navbar() {
             </GridItem>
           </Grid>
         </GridItem>
-        <GridItem
-          width={40}
-          alignSelf="center"
-          justifySelf="center"
-          align="right"
-        >
+
+        <GridItem>
           <Link
             target="_blank"
             rel="noopener noreferrer"
-            href="https://api.whatsapp.com/send?phone=5583982301530&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+a+We+Make+-+N%C3%B3s+criamos%21"
+            href="https:api.whatsapp.com/send?phone=5583982301530&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+a+We+Make+-+N%C3%B3s+criamos%21"
           >
             <Button
               color="white"
@@ -65,6 +75,68 @@ export default function Navbar() {
           </Link>
         </GridItem>
       </Grid>
+
+      {/* <Grid */}
+      {/*   templateColumns={{ lg: 'auto 1fr auto', base: 'auto auto' }} */}
+      {/*   gap={8} */}
+      {/*   height={20} */}
+      {/* > */}
+      {/*   <GridItem width={40} alignSelf="center" justifySelf="start"> */}
+      {/*     <Link href="/"> */}
+      {/*       <Image */}
+      {/*         src="/wemake-logo.png" */}
+      {/*         alt="We Make - nós criamos" */}
+      {/*         width={135} */}
+      {/*         height={45} */}
+      {/*       /> */}
+      {/*     </Link> */}
+      {/*   </GridItem> */}
+      {/*   <GridItem alignSelf="center" display={{ base: 'none', lg: 'grid' }}> */}
+      {/*     <Grid templateColumns="repeat(5, auto)" gap={9}> */}
+      {/*       <GridItem> */}
+      {/*         <Link href="/home">Quem Somos</Link> */}
+      {/*       </GridItem> */}
+      {/*       <GridItem> */}
+      {/*         <Link href="/home">Filosofia Educacional</Link> */}
+      {/*       </GridItem> */}
+      {/*       <GridItem> */}
+      {/*         <Link href="/home">Makerspace</Link> */}
+      {/*       </GridItem> */}
+      {/*       <GridItem> */}
+      {/*         <Link href="/home">Nova Língua</Link> */}
+      {/*       </GridItem> */}
+      {/*       <GridItem> */}
+      {/*         <Link href="/home">Soluções</Link> */}
+      {/*       </GridItem> */}
+      {/*     </Grid> */}
+      {/*   </GridItem> */}
+      {/*   <GridItem */}
+      {/*     width={40} */}
+      {/*     alignSelf="center" */}
+      {/*     justifySelf="center" */}
+      {/*     align="right" */}
+      {/*   > */}
+      {/*     <Link */}
+      {/*       target="_blank" */}
+      {/*       rel="noopener noreferrer" */}
+      {/*       href="https://api.whatsapp.com/send?phone=5583982301530&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+a+We+Make+-+N%C3%B3s+criamos%21" */}
+      {/*     > */}
+      {/*       <Button */}
+      {/*         color="white" */}
+      {/*         bgGradient="linear(to-r, gradient.darkBlue, gradient.lightBlue)" */}
+      {/*         fontWeight="400" */}
+      {/*         fontFamily="Manrope" */}
+      {/*         transition="background-color 0.5s ease" */}
+      {/*         _hover={{ */}
+      {/*           bgGradient: */}
+      {/*             'linear(to-l, gradient.darkBlue, gradient.lightBlue)', */}
+      {/*         }} */}
+      {/*       > */}
+      {/*         Fale Conosco */}
+      {/*       </Button> */}
+      {/*     </Link> */}
+      {/*   </GridItem> */}
+      {/* </Grid> */}
     </Container>
   )
 }
