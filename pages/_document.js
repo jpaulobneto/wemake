d66b29cd2bb28c9d8b2fb44501a724844a789ba7
João Paulo Barbosa <jpaulobneto@gmail.com>
2023-01-18 22:17:54 -0300

    feat: add google analytics script

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
