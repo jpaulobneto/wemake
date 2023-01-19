3681da5c6a56454d10bb7791bf3ae6c4cdfde1a3
João Paulo <jpaulobneto@gmail.com>
2023-01-18 22:18:58 -0300

    feat: add google analytics script (#23)

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,54 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
+import theme from '/theme'
+
 import '@fontsource/manrope/600.css'
 import '@fontsource/raleway/300.css'
 import '@fontsource/raleway/700.css'
 import '@fontsource/sora/400.css'
 import '@fontsource/sora/700.css'
-import Head from 'next/head'
-import theme from '/theme'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
