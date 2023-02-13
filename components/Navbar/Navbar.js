6afdbd2b621b2e95d8027dca3c9227daf0504486
João Paulo <jpaulobneto@gmail.com>
2023-02-13 12:02:56 -0300

    fix: whatsapp uri (#29)

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -70,7 +70,7 @@ export default function Navbar() {
             <Link
               target="_blank"
               rel="noopener noreferrer"
-              href="https:api.whatsapp.com/send?phone=5583982301530&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+a+We+Make+-+N%C3%B3s+criamos%21"
+              href="https://api.whatsapp.com/send?phone=5583982301530&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+a+We+Make+-+N%C3%B3s+criamos%21"
             >
               <Button
                 color="white"
